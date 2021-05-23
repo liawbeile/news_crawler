888562c751b29fcdf3535a237b686e51b319a182
liawbeile <liawbeile@hotmail.com>
2021-05-23 13:07:55 +0800

    improved custom functions

--- ms_suggested_articles.py
+++ ms_suggested_articles.py
@@ -9,40 +9,36 @@ ms_page=req.get("https://mothership.sg/")
 ms_soup = BeautifulSoup(ms_page.text, 'html.parser')
 latest_news = ms_soup.find('div', id='latest-news')
 
-# Retrieve latest news articles' titles
-def title_of_latest_news(article):
+# Retrieve latest news articles' titles and captions
+def title_of_news(article):
     for headlines in article.find_all("div", class_="header"):
-        print("\n\n")
-        print("Latest news: ")
-        print('--------')
         print(headlines.find('h1').text)
-        print('--------')
-        print("\n")
+        print(headlines.find('p').text)
 
 # Retrieve suggested posts' URL, titles and caption
 def suggested_posts_of_latest_news(article):
-        for link in article.find_all('a'):
-            #request the URL of each of the latest news article
-            article_page=req.get(link.get('href'))
-            #parse through the texts within the page that the URL brings us to
-            ms_art_soup = BeautifulSoup(article_page.text, 'html.parser')
-            #filter text by related articles > latest news > ind-article 
-            related_news = ms_art_soup.find('div', class_='related-articles')
-            for latest_related_article in related_news.find_all('div', id='latest-news'):
-                print("Suggested articles for latest news article: ")
-                print('--------')
-                for related_article in latest_related_article.find_all('div', class_='ind-article'):
-                    #print the URL of related articles
-                    for related_art_link in related_article.find_all('a'):
-                        print(related_art_link.get('href'))
-                    #print the header and caption of the related articles
-                    for related_post in related_article.find_all("div", class_="container loaded-post-container"):
-                        for related_headlines in related_post.find_all('div', class_='header'):
-                            print(related_headlines.find('h1').text)
-                            print(related_headlines.find('p').text)
-                            print('--------')
+    for link in article.find_all('a'):
+        article_page=req.get(link.get('href')) #request the URL of each of the latest news article
+        ms_art_soup = BeautifulSoup(article_page.text, 'html.parser') #parse through the texts within the page that the URL brings us to
+        related_news = ms_art_soup.find('div', class_='related-articles') #filter text by related articles > latest news > ind-article 
+        for latest_related_article in related_news.find_all('div', id='latest-news'):
+            for related_article in latest_related_article.find_all('div', class_='ind-article'): #print the URL of related articles
+                for related_art_link in related_article.find_all('a'):
+                    print(related_art_link.get('href'))
+                for related_post in related_article.find_all("div", class_="container loaded-post-container"): #print the header and caption of the related articles
+                    title_of_news(related_post)
+                    print('--------')
 
 # For all latest news articles, request its article URL link and retrieve all suggested posts at the bottom of the article page
-for article in latest_news.find_all("div", class_="ind-article"):
-    title_of_latest_news(article)
-    suggested_posts_of_latest_news(article)
+def main(latest_news):
+    for article in latest_news.find_all("div", class_="ind-article"):
+        print("\n\n")
+        print("Latest news: ")
+        print('--------')
+        title_of_news(article)
+        print('--------')
+        print("\n")
+        print("Suggested articles for latest news article: ")
+        suggested_posts_of_latest_news(article)
+        
+main(latest_news)
